cd61b24f9e6d239da52e99379380999771ee84b9
Mikkel Denker <mikkel@cuely.io>
2022-05-23 16:26:10 +0200

    distance calculations

--- src/webgraph/memory_store.rs
+++ src/webgraph/memory_store.rs
@@ -1,19 +1,21 @@
 use super::{Edge, Node};
 use crate::webgraph::GraphStore;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 
 type NodeName = String;
 type NodeID = u64;
 
+#[derive(Debug)]
 struct InternalEdge {
     to_node: NodeID,
     label: String,
 }
 
 #[derive(Default)]
-pub(crate) struct MemoryStore {
-    adjacency: BTreeMap<NodeID, Vec<InternalEdge>>,
-    node2id: BTreeMap<NodeName, NodeID>,
+pub struct MemoryStore {
+    adjacency: HashMap<NodeID, Vec<InternalEdge>>,
+    reversed_adjacency: HashMap<NodeID, Vec<InternalEdge>>,
+    node2id: HashMap<NodeName, NodeID>,
     id2node: Vec<NodeName>,
     next_id: NodeID,
 }
@@ -44,7 +46,7 @@ impl GraphStore for MemoryStore {
             Some(id) => self
                 .adjacency
                 .get(id)
-                .expect("Fatal error: node2id and adjacency are out of sync")
+                .unwrap_or(&Vec::new())
                 .clone()
                 .into_iter()
                 .map(|internal_edge| {
@@ -84,8 +86,43 @@ impl GraphStore for MemoryStore {
             .or_default()
             .push(InternalEdge {
                 to_node: to_id,
+                label: edge.label.clone(),
+            });
+
+        self.reversed_adjacency
+            .entry(to_id)
+            .or_default()
+            .push(InternalEdge {
+                to_node: from_id,
                 label: edge.label,
             });
+    }
+
+    fn ingoing_edges(&self, node: Node) -> Vec<Edge> {
+        match self.node2id.get(&node.name) {
+            None => Vec::new(),
+            Some(id) => self
+                .reversed_adjacency
+                .get(id)
+                .unwrap_or(&Vec::new())
+                .clone()
+                .into_iter()
+                .map(|internal_edge| {
+                    let from_name = self
+                        .id2node
+                        .get(internal_edge.to_node as usize)
+                        .expect("Fatal error: id2node out of sync")
+                        .clone();
+                    let from = Node { name: from_name };
+
+                    Edge {
+                        from,
+                        to: node.clone(),
+                        label: internal_edge.label.clone(),
+                    }
+                })
+                .collect(),
+        }
     }
 }
 
@@ -161,10 +198,33 @@ mod test {
             },]
         );
         assert_eq!(
-            store.outgoing_edges(c.clone()),
+            store.ingoing_edges(c.clone()),
+            vec![
+                Edge {
+                    from: b.clone(),
+                    to: c.clone(),
+                    label: String::new()
+                },
+                Edge {
+                    from: a.clone(),
+                    to: c.clone(),
+                    label: String::new()
+                },
+            ]
+        );
+        assert_eq!(
+            store.ingoing_edges(a.clone()),
             vec![Edge {
                 from: c.clone(),
                 to: a.clone(),
+                label: String::new()
+            },]
+        );
+        assert_eq!(
+            store.ingoing_edges(b.clone()),
+            vec![Edge {
+                from: a.clone(),
+                to: b.clone(),
                 label: String::new()
             },]
         );
--- src/webgraph/mod.rs
+++ src/webgraph/mod.rs
@@ -1,26 +1,176 @@
 mod memory_store;
 mod sled_store;
+use std::cmp;
+use std::collections::{BinaryHeap, HashMap};
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+use itertools::TakeWhileRef;
+use memory_store::MemoryStore;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Node {
     name: String,
+}
+
+impl From<String> for Node {
+    fn from(name: String) -> Self {
+        Self { name }
+    }
+}
+
+impl From<&str> for Node {
+    fn from(name: &str) -> Self {
+        Self::from(name.to_string())
+    }
 }
 
 pub trait GraphStore {
     type Iter: Iterator<Item = Node>;
 
     fn outgoing_edges(&self, node: Node) -> Vec<Edge>;
+    fn ingoing_edges(&self, node: Node) -> Vec<Edge>;
     fn nodes(&self) -> Self::Iter;
     fn insert(&mut self, edge: Edge);
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Edge {
     from: Node,
     to: Node,
     label: String,
 }
 
+impl Edge {
+    pub fn new(from: Node, to: Node, label: String) -> Self {
+        Edge { from, to, label }
+    }
+}
+
 pub struct WebGraph<S: GraphStore> {
     internal_store: S,
+}
+
+impl<S: GraphStore> WebGraph<S> {
+    pub fn new_memory() -> WebGraph<MemoryStore> {
+        WebGraph {
+            internal_store: MemoryStore::default(),
+        }
+    }
+
+    pub fn insert(&mut self, edge: Edge) {
+        self.internal_store.insert(edge)
+    }
+
+    fn dijkstra<F1, F2>(&self, source: Node, node_edges: F1, edge_node: F2) -> HashMap<Node, usize>
+    where
+        F1: Fn(Node) -> Vec<Edge>,
+        F2: Fn(&Edge) -> Node,
+    {
+        let mut distances: HashMap<Node, usize> = HashMap::default();
+        let mut queue = BinaryHeap::new();
+
+        queue.push(cmp::Reverse((0 as usize, source.clone())));
+        distances.insert(source.clone(), 0);
+
+        while let Some(state) = queue.pop() {
+            let (cost, v) = state.0;
+            let current_dist = distances.get(&v).unwrap_or(&usize::MAX);
+
+            if cost > *current_dist {
+                continue;
+            }
+
+            for edge in node_edges(v) {
+                if cost + 1 < *distances.get(&edge_node(&edge)).unwrap_or(&usize::MAX) {
+                    let next = cmp::Reverse((cost + 1, edge_node(&edge)));
+                    queue.push(next);
+                    distances.insert(edge_node(&edge), cost + 1);
+                }
+            }
+        }
+
+        distances
+    }
+
+    pub fn distances(&self, source: Node) -> HashMap<Node, usize> {
+        self.dijkstra(
+            source,
+            |node| self.internal_store.outgoing_edges(node),
+            |edge| edge.to.clone(),
+        )
+    }
+
+    pub fn reversed_distances(&self, source: Node) -> HashMap<Node, usize> {
+        self.dijkstra(
+            source,
+            |node| self.internal_store.ingoing_edges(node),
+            |edge| edge.from.clone(),
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn distance_calculation() {
+        //     ┌────┐
+        //     │    │
+        // ┌───A◄─┐ │
+        // │      │ │
+        // ▼      │ │
+        // B─────►C◄┘
+        //        ▲
+        //        │
+        //        │
+        //        D
+
+        let mut graph = WebGraph::<MemoryStore>::new_memory();
+
+        graph.insert(Edge::new(Node::from("A"), Node::from("B"), String::new()));
+        graph.insert(Edge::new(Node::from("B"), Node::from("C"), String::new()));
+        graph.insert(Edge::new(Node::from("A"), Node::from("C"), String::new()));
+        graph.insert(Edge::new(Node::from("C"), Node::from("A"), String::new()));
+        graph.insert(Edge::new(Node::from("D"), Node::from("C"), String::new()));
+
+        let distances = graph.distances(Node::from("D"));
+
+        assert_eq!(distances.get(&Node::from("C")), Some(&1));
+        assert_eq!(distances.get(&Node::from("A")), Some(&2));
+        assert_eq!(distances.get(&Node::from("B")), Some(&3));
+    }
+
+    #[test]
+    fn reversed_distance_calculation() {
+        //     ┌────┐
+        //     │    │
+        // ┌───A◄─┐ │
+        // │      │ │
+        // ▼      │ │
+        // B─────►C◄┘
+        //        ▲
+        //        │
+        //        │
+        //        D
+
+        let mut graph = WebGraph::<MemoryStore>::new_memory();
+
+        graph.insert(Edge::new(Node::from("A"), Node::from("B"), String::new()));
+        graph.insert(Edge::new(Node::from("B"), Node::from("C"), String::new()));
+        graph.insert(Edge::new(Node::from("A"), Node::from("C"), String::new()));
+        graph.insert(Edge::new(Node::from("C"), Node::from("A"), String::new()));
+        graph.insert(Edge::new(Node::from("D"), Node::from("C"), String::new()));
+
+        let distances = graph.reversed_distances(Node::from("D"));
+
+        assert_eq!(distances.get(&Node::from("C")), None);
+        assert_eq!(distances.get(&Node::from("A")), None);
+        assert_eq!(distances.get(&Node::from("B")), None);
+
+        let distances = graph.reversed_distances(Node::from("A"));
+
+        assert_eq!(distances.get(&Node::from("C")), Some(&1));
+        assert_eq!(distances.get(&Node::from("D")), Some(&2));
+        assert_eq!(distances.get(&Node::from("B")), Some(&2));
+    }
 }
