38a15f025395c7a3f3c5fa9d9dd6bbc62ca32c19
Mikkel Denker <mikkel@cuely.io>
2022-09-06 09:12:15 +0200

    query benchmark

---
+++ benches/search-preindexed.rs
@@ -0,0 +1,27 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use cuely::{index::Index, searcher::Searcher};
+
+const INDEX_PATH: &str = "data/index";
+
+macro_rules! bench {
+    ($query:tt, $searcher:ident, $c:ident) => {
+        let mut desc = "search '".to_string();
+        desc.push_str($query);
+        desc.push('\'');
+        $c.bench_function(desc.as_str(), |b| {
+            b.iter(|| $searcher.search($query, None, None, None).unwrap())
+        });
+    };
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let index = Index::open(INDEX_PATH).unwrap();
+    let searcher = Searcher::new(index, None, None);
+
+    bench!("the", searcher, c);
+    bench!("dtu", searcher, c);
+    bench!("the best", searcher, c);
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
