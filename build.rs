9e18189e35ed6ea7cc888d4b2c79658a853bc436
Oliver Bøving <oliver@bvng.dk>
2022-09-08 21:54:37 +0200

    Run `npm install` in build.rs

--- build.rs
+++ build.rs
@@ -3,6 +3,12 @@ use std::process::Command;
 fn main() {
     Command::new("npm")
         .current_dir("frontend")
+        .arg("install")
+        .status()
+        .unwrap();
+
+    Command::new("npm")
+        .current_dir("frontend")
         .args(&["run", "build"])
         .status()
         .unwrap();
