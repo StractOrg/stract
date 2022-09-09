abf8b774c4d9fca80e25933c1b6f143ca11d13ba
Oliver Bøving <oliver@bvng.dk>
2022-09-09 12:12:32 +0200

    Convert privacy and about page to MDX

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()]
 });
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -10,5 +10,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
