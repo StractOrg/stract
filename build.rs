f972b163c5970ef8608d7a627c192d460a55bc8c
Oliver Bøving <oliver@bvng.dk>
2022-09-10 12:23:33 +0200

    Optimize frontend build time (#39)
    This moves building the astro frontend from build.rs into the justfile.
    
    This streamlines the build process for the frontend astro part, and the
    frontend application itself by letting cargo watch rebuild the astro and
    then the Rust binary, instead of building astro in build.rs.
    
    Non-conclusive results says that this improves build times from about
    13s to 6s, while being more consistent :)

--- build.rs
+++ build.rs
@@ -1,17 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    Command::new("npm")
-        .current_dir("frontend")
-        .arg("install")
-        .status()
-        .unwrap();
-
-    Command::new("npm")
-        .current_dir("frontend")
-        .args(&["run", "build"])
-        .status()
-        .unwrap();
-
     lalrpop::process_root().unwrap();
 }
