f988616b8fd740d99a9c2b0648b891590c617666
Oliver Bøving <oliver@bvng.dk>
2022-09-10 21:24:45 +0200

    Use faker data in askama templates during dev
    By adding a `$ {{lorem.limes}}` for example to askama`...` expressions,
    fake data is inserted during development.
    
    Additionally all askama specific text (such as {% for x in xs %}) is
    not produced during development for cleaner pages.

--- frontend/src/askama.ts
+++ frontend/src/askama.ts
@@ -1,25 +1,58 @@
+import { faker } from "@faker-js/faker";
+
 type Component = (() => any) | any;
 
 const prepare = (c: Component | null) => (typeof c == "function" ? c() : c);
 
-const match = (expr: string, cases: Record<string, Component>) =>
-  [
+const passthrough = import.meta.env.DEV;
+
+const fake = (prod: any, dev: any) =>
+  passthrough ? faker.helpers.fake(dev) : prod;
+const match = (expr: string, cases: Record<string, Component>) => {
+  if (passthrough) {
+    const options = Object.values(cases);
+    return prepare(options[Math.floor(Math.random() * options.length)]);
+  }
+
+  return [
     `{% match ${expr} %}`,
     Object.entries(cases).map(([c, v]) => [`{% when ${c} %}`, prepare(v)]),
+    ,
     `{% endmatch %}`,
   ].flat(100);
+};
 type ArrayPattern<S> = S extends `${infer _} in ${infer _}` ? S : never;
-const for_ = <S extends string>(pat: ArrayPattern<S>, f: Component) =>
-  [`{% for ${pat} %}`, prepare(f), `{% endfor %}`].flat(100);
-const if_ = (pat: string, then: Component, else_: null | Component = null) =>
-  [
+const for_ = <S extends string>(pat: ArrayPattern<S>, f: Component) => {
+  if (passthrough) {
+    const n = Math.floor(Math.random() * 6);
+    return Array.from({ length: n })
+      .map(() => prepare(f))
+      .flat(100);
+  }
+  return [`{% for ${pat} %}`, prepare(f), `{% endfor %}`].flat(100);
+};
+const if_ = (pat: string, then: Component, else_: null | Component = null) => {
+  if (passthrough) {
+    return Math.random() > 0.5 ? prepare(then) : prepare(else_);
+  }
+
+  return [
     `{% if ${pat} %}`,
     prepare(then),
     else_ && [`{% else %}`, prepare(else_)],
     `{% endif %}`,
   ].flat(100);
+};
 
 export const askama = Object.assign(
-  (s: TemplateStringsArray) => `{{ ${s.raw.join("")} }}`,
-  { match, for_, if_ }
+  (input: TemplateStringsArray) => {
+    const str = input.raw.join("");
+    const segmetns = str.split("$");
+    if (passthrough && segmetns.length > 1) {
+      return faker.helpers.fake(segmetns.slice(1).join("$"));
+    } else {
+      return `{{ ${segmetns[0]} }}`;
+    }
+  },
+  { fake, match, for_, if_ }
 );
