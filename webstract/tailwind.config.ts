336081c81d086bc868cb87019482406487246138
Oliver Bøving <oliver@bvng.dk>
2023-08-28 12:23:42 +0200

    Add placeholder Tailwind config for VSCode intellisense

--- webstract/tailwind.config.ts
+++ webstract/tailwind.config.ts
@@ -1,2 +1,16 @@
 // NOTE: This file exists only to get the Tailwind VSCode extension to kick in.
 //       See https://github.com/denoland/fresh/issues/1519 for details.
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "rgb(11 123 255 / <alpha-value>)",
+        brand_contrast: "rgb(255 105 11 / <alpha-value>)",
+        snippet: "#4d5156",
+      },
+    },
+  },
+};
