dfccb757f25273c4cf584bd23c608ff649cf206d
Oliver Bøving <oliver@bvng.dk>
2023-09-02 18:17:58 +0200

    Refactor manage optics UI

--- webstract/islands/ManageOptics.tsx
+++ webstract/islands/ManageOptics.tsx
@@ -5,7 +5,7 @@ import {
   updateStorageSignal,
   useSyncSignalWithLocalStorage,
 } from "../search/utils.ts";
-import { tx } from "https://esm.sh/@twind/core@1.1.3";
+import { HiMinusCircleOutline } from "../icons/HiMinusCircleOutline.tsx";
 
 export const ManageOptics = () => {
   const newOptic = useSignal<OpticOption>({
@@ -16,67 +16,62 @@ export const ManageOptics = () => {
 
   useSyncSignalWithLocalStorage(opticsSignal);
 
-  const inputClass = tx`border-none rounded`;
-
   return (
     <div>
       {IS_BROWSER &&
         (
-          <>
-            <div class="flex w-full justify-between px-5 h-10">
-              <input
-                class={inputClass}
-                id="name"
-                type="text"
-                placeholder="Name"
-                value={newOptic.value.name}
-                onInput={(e) =>
-                  newOptic.value.name = (e.target as HTMLInputElement).value}
-              />
-              <input
-                class={inputClass}
-                id="url"
-                type="text"
-                placeholder="Url"
-                value={newOptic.value.url}
-                onInput={(e) =>
-                  newOptic.value.url = (e.target as HTMLInputElement).value}
-              />
-              <button
-                class="w-20 rounded-full border-0 bg-brand text-sm text-white"
-                id="add-btn"
-                onClick={() => {
-                  updateStorageSignal(
-                    opticsSignal,
-                    (optics) => [...optics, newOptic.value],
-                  );
-                  newOptic.value = {
-                    name: "",
-                    url: "",
-                    description: "",
-                  };
-                }}
-              >
-                Add
-              </button>
-            </div>
-            <div class="flex w-full px-5 h-10 mt-2">
-              <input
-                class={inputClass}
-                id="description"
-                type="text"
-                placeholder="Description"
-                value={newOptic.value.description}
-                onInput={(e) =>
-                  newOptic.value.description =
-                    (e.target as HTMLInputElement).value}
-              />
-            </div>
-          </>
+          <form
+            class="grid w-full px-8 grid-cols-[1fr_1fr_5rem] gap-x-5 gap-y-2 [&>input]:border-none [&>input]:rounded"
+            onSubmit={(e) => {
+              e.preventDefault();
+              updateStorageSignal(
+                opticsSignal,
+                (optics) => [...optics, newOptic.value],
+              );
+              newOptic.value = {
+                name: "",
+                url: "",
+                description: "",
+              };
+            }}
+          >
+            <input
+              type="text"
+              required
+              placeholder="Name"
+              value={newOptic.value.name}
+              onInput={(e) =>
+                newOptic.value.name = (e.target as HTMLInputElement).value}
+            />
+            <input
+              type="text"
+              required
+              placeholder="Url"
+              value={newOptic.value.url}
+              onInput={(e) =>
+                newOptic.value.url = (e.target as HTMLInputElement).value}
+            />
+            <button
+              class="rounded-full border-0 bg-brand text-sm text-white"
+              title="Remove optic"
+            >
+              Add
+            </button>
+            <input
+              class="col-span-2"
+              type="text"
+              required
+              placeholder="Description"
+              value={newOptic.value.description}
+              onInput={(e) =>
+                newOptic.value.description =
+                  (e.target as HTMLInputElement).value}
+            />
+          </form>
         )}
       <div class="mt-5">
         <div
-          class="grid grid-cols-[auto_1fr_1fr_1fr] w-full gap-5"
+          class="grid grid-cols-[auto_1fr_2fr_auto] w-full gap-5"
           id="optics-list"
         >
           <span />
@@ -86,24 +81,25 @@ export const ManageOptics = () => {
           {[
             ...opticsSignal.value.data.map((optic) => ({
               optic,
-              img: "/images/delete.svg",
+              removable: true,
             })),
             ...DEFAULT_OPTICS.map((optic) => ({
               optic,
-              img: "/images/disabled-delete.svg",
+              removable: false,
             })),
-          ].map(({ optic, img }) => (
+          ].map(({ optic, removable }) => (
             <>
-              <div
-                class="w-5"
+              <button
+                class="w-6 !bg-transparent flex items-start group"
                 onClick={() =>
                   updateStorageSignal(
                     opticsSignal,
                     (optics) => optics.filter((o) => o != optic),
                   )}
+                disabled={!removable}
               >
-                <img src={img} class="h-5 w-5" />
-              </div>
+                <HiMinusCircleOutline class="group-enabled:text-red-500 group-enabled:group-hover:text-red-400 transition" />
+              </button>
               <div class="text-sm">{optic.name}</div>
               <div class="text-sm">
                 {optic.description}
