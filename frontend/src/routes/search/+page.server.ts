01caea12d6b8a4f0705f3409e1d0ba892c147389
Oliver Bøving <oliver@bvng.dk>
2023-09-25 12:17:36 +0200

    Remove two instances of @ts-ignore by defining the locals in app.d.ts

--- frontend/src/routes/search/+page.server.ts
+++ frontend/src/routes/search/+page.server.ts
@@ -2,7 +2,6 @@ import { extractSearchParams, type SearchParams } from '$lib/search';
 import type { Actions } from './$types';
 
 export const load = async ({ locals }) => {
-  // @ts-ignore
   const searchParams: SearchParams | undefined =
     (locals['form'] && extractSearchParams(locals['form'])) || undefined;
   return { form: searchParams };
@@ -12,7 +11,6 @@ export const actions: Actions = {
   default: async (event) => {
     const { request } = event;
 
-    // @ts-ignore
     event.locals.form = await request.formData();
 
     return { success: true };
