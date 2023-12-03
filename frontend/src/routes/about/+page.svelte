00107b23e29d3f6aafcc26d999a0d5981bd8b184
Mikkel Denker <mikkel@trystract.com>
2023-12-03 16:21:59 +0100

    add discord and matrix link to about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
   import Article from '$lib/components/Article.svelte';
   import type { PageData } from './$types';
+  import IconDiscord from '~icons/simple-icons/discord';
+  import IconMatrix from '~icons/simple-icons/matrix';
+  import Link from '$lib/components/Link.svelte';
 
   export let data: PageData;
 </script>
 
 <Article>
   {@html data.md}
+
+  <div class="flex justify-center mt-10">
+    <div class="flex space-x-10">
+      <Link href="https://discord.gg/BmzKHffWJM" label="Join our Discord server" round>
+        <IconDiscord />
+      </Link>
+      <Link href="https://matrix.to/#/#stract:matrix.org" label="Join our Matrix server" round>
+        <IconMatrix />
+      </Link>
+    </div>
+  </div>
 </Article>
