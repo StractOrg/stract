9e1bb23a9613360b8827a72fe8cee317d000c522
Mikkel Denker <mikkel@trystract.com>
2023-12-20 07:37:26 +0100

    use static env variables in frontend https://kit.svelte.dev/docs/migrating-to-sveltekit-2#dynamic-environment-variables-cannot-be-used-during-prerendering

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,8 +1,8 @@
-import { env } from '$env/dynamic/public';
+import { PUBLIC_API_BASE } from '$env/static/public';
 import { setGlobalApiBase } from '$lib/api';
 import { globals } from '$lib/globals';
 import type { LayoutLoad } from './$types';
 
-setGlobalApiBase(env.PUBLIC_API_BASE || 'http://0.0.0.0:3000');
+setGlobalApiBase(PUBLIC_API_BASE || 'http://0.0.0.0:3000');
 
 export const load: LayoutLoad = async () => ({ globals: await globals() });
