99a2c3ba9f2cce061d5fcf905113457c54bcb489
Wes Appler <wes@lamemakes.com>
2024-02-26 22:30:06 -0500

    More CI attempts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite';
 import wasm from 'vite-plugin-wasm';
+import wasmPack from 'vite-plugin-wasm-pack';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
   plugins: [
+    wasmPack("../crates/client-wasm"),
     wasm(),
     topLevelAwait(),
     sveltekit(),
