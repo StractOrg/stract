2aa49cb8c0efae6a9c3ae119fad2d158306bd566
Mikkel Denker <mikkel@stract.com>
2024-05-28 11:40:24 +0200

    hopefully fix ci

--- tools/ranking-diff/svelte.config.js
+++ tools/ranking-diff/svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({
+    style: {
+      css: {
+        postcss: join(__dirname, 'postcss.config.js'),
+      },
+    },
+  }),
 
   kit: {
     csrf: {
