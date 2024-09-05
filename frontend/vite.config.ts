9d0dba7da25dc4ad54333bba2bffa8f1598b01c9
Mikkel Denker <mikkel@stract.com>
2024-09-05 10:49:15 +0200

    move captcha images/audio to correct location during build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import Icons from 'unplugin-icons/vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasmPack from './src/wasm-pack-plugin';
+import copy from 'rollup-plugin-copy';
+import path from 'path';
+import { normalizePath } from 'vite';
 
 export default defineConfig({
   plugins: [
@@ -14,6 +17,19 @@ export default defineConfig({
     topLevelAwait(),
     sveltekit(),
     Icons({ compiler: 'svelte' }),
+    copy({
+      targets: [
+        {
+          src: normalizePath(path.resolve(__dirname, './src/lib/captcha/images')),
+          dest: normalizePath(path.resolve(__dirname, './build/server/chunks')),
+        },
+        {
+          src: normalizePath(path.resolve(__dirname, './src/lib/captcha/audio')),
+          dest: normalizePath(path.resolve(__dirname, './build/server/chunks')),
+        },
+      ],
+      hook: 'closeBundle',
+    }),
   ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
